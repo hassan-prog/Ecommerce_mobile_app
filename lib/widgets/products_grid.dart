990624af5df0b2d520891408482d65357195ce0c
hassan-prog <alielsayed1112@gmail.com>
2023-01-24 05:13:29 +0200

    products provider renamed to "products"

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/products_provider.dart';
+import '../providers/products.dart';
 import './product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
@@ -11,7 +11,7 @@ class ProductsGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productData = Provider.of<ProductsProvider>(context);
+    final productData = Provider.of<Products>(context);
     final products =
         showFavoriteProducts ? productData.favoriteItems : productData.items;
 
